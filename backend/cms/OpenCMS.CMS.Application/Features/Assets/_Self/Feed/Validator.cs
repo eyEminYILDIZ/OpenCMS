0c7f55904f929de7a839435ac6e5809ea1899728
Emin YILDIZ <eyeminyildiz@outlook.com>
2026-05-18 12:01:14 +0300

    Backend: Created validator for Agents/Feed command

---
+++ backend/cms/OpenCMS.CMS.Application/Features/Assets/_Self/Feed/Validator.cs
@@ -0,0 +1,39 @@
+using FluentValidation;
+
+namespace OpenCMS.CMS.Application.Assets.Self.Feed;
+
+public class Validator : AbstractValidator<Command>
+{
+    public Validator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Asset ID is required.");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MaximumLength(200).WithMessage("Name must not exceed 200 characters.");
+
+        RuleFor(x => x.Latitude)
+            .InclusiveBetween(-90, 90).WithMessage("Latitude must be between -90 and 90.");
+
+        RuleFor(x => x.Longitude)
+            .InclusiveBetween(-180, 180).WithMessage("Longitude must be between -180 and 180.");
+
+        // TODO: for underground assets altitude can be negative, so we need to allow that. 
+        // We can add a new property to indicate if the asset is underground or not, and then validate altitude accordingly.
+        RuleFor(x => x.Altitude)
+            .GreaterThanOrEqualTo(0).WithMessage("Altitude must be non-negative.");
+
+        RuleFor(x => x.Heading)
+            .InclusiveBetween(0, 360).WithMessage("Heading must be between 0 and 360 degrees.");
+
+        RuleFor(x => x.Speed)
+            .GreaterThanOrEqualTo(0).WithMessage("Speed must be non-negative.");
+
+        RuleFor(x => x.AssetType)
+            .IsInEnum().WithMessage("AssetType is not a valid value.");
+
+        RuleFor(x => x.ThreatType)
+            .IsInEnum().WithMessage("ThreatType is not a valid value.");
+    }
+}
