932ed4d5419919282d9391e6577390a7dfe6e7f4
Emin YILDIZ <eyeminyildiz@outlook.com>
2026-06-01 13:45:16 +0300

    Backend: Created load and stress tests with K6

---
+++ backend/cms/OpenCMS.CMS.AgentApi.Tests/readme.md
@@ -0,0 +1,22 @@
+# Testing
+
+Make sure you installed K6 on the system.
+
+## Load Testing
+
+Expected to run in normal conditions.
+
+To run load testing:
+```sh
+k6 run load_test.js
+```
+
+
+## Stress Testing
+
+Expected to break at some point.
+
+To run stress testing:
+```sh
+k6 run stress_test.js
+```
---
+++ backend/cms/OpenCMS.CMS.AgentApi.Tests/stress_test.js
@@ -0,0 +1,31 @@
+/////////////////////////////////////////////////////////////////////////////////
+////////////////////// K6 LOAD TESTING //////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+// Test configuration
+export const options = {
+    thresholds: {
+        // Assert that 99% of requests finish within 5.000ms.
+        http_req_duration: ["p(99) < 5000"],
+    },
+    // Ramp the number of virtual users up and down
+    stages: [
+        { duration: "10s", target: 1000 },
+        { duration: "30s", target: 5000 },
+        { duration: "30s", target: 10000 },
+        { duration: "30s", target: 100000 },
+        { duration: "30s", target: 5000 },
+        { duration: "10s", target: 0 },
+    ],
+};
+
+// Simulated user behavior
+export default function () {
+    let res = http.get("http://localhost:5010/assets");
+    // Validate response status
+    check(res, { "status was 200": (r) => r.status == 200 });
+    sleep(1);
+}
