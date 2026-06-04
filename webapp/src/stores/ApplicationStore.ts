a2945cd557b52458d93f4a33432969083f670cd4
Emin YILDIZ <eyeminyildiz@outlook.com>
2026-06-04 23:34:22 +0300

    WebApp: Created agent and operation count api call flows

--- webapp/src/stores/ApplicationStore.ts
+++ webapp/src/stores/ApplicationStore.ts
@@ -1,6 +1,6 @@
 import { makeAutoObservable, toJS } from "mobx";
 import { ApiClient } from "../api/axios_setup";
-import { AssetApi } from "../api";
+import { AgentApi, AssetApi, OperationApi } from "../api";
 
 export class ApplicationStore {
     constructor() {
@@ -8,6 +8,8 @@ export class ApplicationStore {
     }
 
     assetItemCounts: AssetApi.GetItemCounts.Response | null = null;
+    agentItemCounts: AgentApi.GetItemCounts.Response | null = null;
+    operationItemCounts: OperationApi.GetItemCounts.Response | null = null;
 
     loadItemCounts = async () => {
         try {
@@ -16,7 +18,25 @@ export class ApplicationStore {
 
             console.log(toJS(this.assetItemCounts));
         } catch (error) {
-            console.error("Error loading item counts:", error);
+            console.error("Error loading asset item counts:", error);
+        }
+
+        try {
+            const response = await AgentApi.GetItemCounts.call();
+            this.agentItemCounts = response.data;
+
+            console.log(toJS(this.agentItemCounts));
+        } catch (error) {
+            console.error("Error loading agent item counts:", error);
+        }
+
+        try {
+            const response = await OperationApi.GetItemCounts.call();
+            this.operationItemCounts = response.data;
+
+            console.log(toJS(this.operationItemCounts));
+        } catch (error) {
+            console.error("Error loading operation item counts:", error);
         }
     }
 }
