8e5ea28d1c003227de6990cee274be939ffa7c2d
Emin YILDIZ <eyeminyildiz@outlook.com>
2026-06-21 13:44:36 +0300

    Backend: Added search support to Assets/ListAll endpoint

--- backend/cms/OpenCMS.CMS.Application/Features/Assets/_Self/ListAll/Endpoint.cs
+++ backend/cms/OpenCMS.CMS.Application/Features/Assets/_Self/ListAll/Endpoint.cs
@@ -1,12 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace OpenCMS.CMS.Application.Assets.Self.ListAll;
 
 public class Endpoint : IClientEndpoint, IAgentEndpoint
 {
     public RouteHandlerBuilder MapEndpoint(WebApplication app)
     {
-        return app.MapGet("/assets", async (IMediator mediator) =>
+        return app.MapGet("/assets", async (IMediator mediator, [FromQuery] string? search) =>
         {
-            var result = await mediator.Send(new Query());
+            var result = await mediator.Send(new Query()
+            {
+                SearchValue = search ?? ""
+            });
             return result.ToHttpResult();
         });
     }
--- backend/cms/OpenCMS.CMS.Application/Features/Assets/_Self/ListAll/Handler.cs
+++ backend/cms/OpenCMS.CMS.Application/Features/Assets/_Self/ListAll/Handler.cs
@@ -11,7 +11,14 @@ public class Handler : IRequestHandler<Query, Result<List<QueryResponse>>>
 
     public async Task<Result<List<QueryResponse>>> Handle(Query request, CancellationToken cancellationToken)
     {
-        var assets = await _dbContext.Assets.ToListAsync(cancellationToken);
+        var assetQuery = _dbContext.Assets.AsQueryable();
+
+        if (!string.IsNullOrEmpty(request.SearchValue))
+        {
+            assetQuery = assetQuery.Where(a => a.Name.ToLower().Contains(request.SearchValue.ToLower()));
+        }
+
+        var assets = await assetQuery.ToListAsync(cancellationToken);
 
         return assets.Select(asset => new QueryResponse
         {
