8379bb84e8e763eb5fc9869ea7ad4f76107fd011
Emin YILDIZ <eyeminyildiz@outlook.com>
2026-06-27 21:22:48 +0300

    MobileApp: Created SignalR web socket service based on webapp

---
+++ mobileapp/src/services/ClientSocketService.ts
@@ -0,0 +1,38 @@
+import * as signalR from '@microsoft/signalr';
+import { Platform } from 'react-native';
+import { AssetApi } from '../api';
+
+const HUB_HOST = Platform.OS === 'android' ? '10.0.2.2' : 'localhost';
+
+type AssetUpdatedHandler = (asset: AssetApi.ListAll.Response) => void;
+
+class ClientSocketService {
+    private connection: signalR.HubConnection;
+
+    constructor() {
+        this.connection = new signalR.HubConnectionBuilder()
+            .withUrl(`http://${HUB_HOST}:5020/hubs/clients`)
+            .withAutomaticReconnect()
+            .build();
+    }
+
+    async start(): Promise<void> {
+        if (this.connection.state === signalR.HubConnectionState.Disconnected) {
+            await this.connection.start();
+        }
+    }
+
+    async stop(): Promise<void> {
+        await this.connection.stop();
+    }
+
+    onAssetUpdated(handler: AssetUpdatedHandler): void {
+        this.connection.on('UpdateAsset', handler);
+    }
+
+    offAssetUpdated(handler: AssetUpdatedHandler): void {
+        this.connection.off('UpdateAsset', handler);
+    }
+}
+
+export const clientSocketService = new ClientSocketService();
