3812f84c697972304023dde268915183b9b894cf
Emin YILDIZ <eyeminyildiz@outlook.com>
2026-07-04 11:21:38 +0300

    Backend: Created Dispatch entity

---
+++ backend/cms/OpenCMS.CMS.Domain/Entities/Dispatch.cs
@@ -0,0 +1,20 @@
+﻿namespace OpenCMS.CMS.Domain.Entities;
+
+public class Dispatch : BaseEntity
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DispatchCategories Category { get; set; }
+    public DateTime OccuredAt { get; set; }
+
+    public Guid RelatedEntityId { get; set; }
+    public Guid? RelatedChildEntityId { get; set; }
+}
+
+public enum DispatchCategories
+{
+    General = 0,
+    Asset = 1,
+    Agent = 2,
+    Operation = 3,
+}
