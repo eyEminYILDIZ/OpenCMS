9118880dd308345813444f67dbe6e477f5afd766
Emin YILDIZ <eyeminyildiz@outlook.com>
2026-07-19 23:50:41 +0300

    Backend: Created KeyboardInputController

---
+++ backend/libraries/OpenCMS.Libraries.InputController/Keyboard/KeyboardInputController.cs
@@ -0,0 +1,56 @@
+using OpenCMS.Libraries.Common.Models;
+
+namespace OpenCMS.Libraries.InputController.Keyboard;
+
+public class KeyboardInputController
+{
+    public (ActuatorActionTypes Action, double Value) ProcessInput()
+    {
+        var readKey = Console.ReadKey(intercept: true);
+        if (readKey.Key == ConsoleKey.UpArrow)
+        {
+            System.Console.WriteLine("Moving Forward");
+            return (ActuatorActionTypes.MoveForward, 1.0);
+        }
+        else if (readKey.Key == ConsoleKey.DownArrow)
+        {
+            System.Console.WriteLine("Moving Backward");
+            return (ActuatorActionTypes.MoveBackward, 1.0);
+        }
+        else if (readKey.Key == ConsoleKey.LeftArrow)
+        {
+            System.Console.WriteLine("Turning Left");
+            return (ActuatorActionTypes.TurnLeft, 1.0);
+        }
+        else if (readKey.Key == ConsoleKey.RightArrow)
+        {
+            System.Console.WriteLine("Turning Right");
+            return (ActuatorActionTypes.TurnRight, 1.0);
+        }
+        else if (readKey.Key == ConsoleKey.PageUp)
+        {
+            System.Console.WriteLine("Moving Up");
+            return (ActuatorActionTypes.MoveUp, 1.0);
+        }
+        else if (readKey.Key == ConsoleKey.PageDown)
+        {
+            System.Console.WriteLine("Moving Down");
+            return (ActuatorActionTypes.MoveDown, 1.0);
+        }
+        else if (readKey.Key == ConsoleKey.A)
+        {
+            System.Console.WriteLine("Opening Autopilot");
+            return (ActuatorActionTypes.OpenAutopilot, 1.0);
+        }
+        else if (readKey.Key == ConsoleKey.B)
+        {
+            System.Console.WriteLine("Closing Autopilot");
+            return (ActuatorActionTypes.CloseAutopilot, 1.0);
+        }
+        else
+        {
+            System.Console.WriteLine($"Key {readKey.Key} pressed. No action assigned.");
+            return (ActuatorActionTypes.None, 0.0);
+        }
+    }
+}
